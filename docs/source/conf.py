a1a2ec0bf1d95f0baa480c7af701dcfb0b5c8d25
John-Pywell <68627694+John-Pywell@users.noreply.github.com>
2024-06-04 15:34:50 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes"]
+
 html_static_path = ['_static']
 
 html_css_files = [
